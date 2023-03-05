3ed3d918de62f79660d6ab51c512e9ba06204cbd
kohikoabe <october.tenth.run@gmail.com>
2023-03-05 22:20:18 +0900

    layoutを作成してindexの表示まで完了

---
+++ myapp/resources/views/layouts/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+  </head>
+  <body>
+    <div class="content">
+      @yield('content')
+    </div>
+    <div class="footer">
+      @yield('footer')
+    </div>
+  </body>
+</html>
